5545cbb11312c72c343f243fcc40c047b6a31e90
leomuso <Pinosenx@gmail.com>
2024-10-15 17:51:22 +0200

    exercise 4.6

--- main.c
+++ main.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
 int main(void) {
-        float area, raggio, circoferenza;
-        raggio = 1;
-        while (raggio<=20) {
-            circoferenza= (2*3.14)*raggio;
-            printf("la circonferenza e': %.2f || ", circoferenza);
-            area = raggio*raggio*(3.14);
-            printf("l'area del cerchio e': %.2f \n", area);
+    float area, raggio /*= 1*/, circoferenza;
+    /*while (raggio<=20) {
+        circoferenza= (2*3.14)*raggio;
+        printf("la circonferenza e': %.2f || ", circoferenza);
+        area = raggio*raggio*(3.14);
+        printf("l'area del cerchio e': %.2f \n", area);
 
-            raggio++;
-        }
-
+        raggio++;
+    }*/
+    for(raggio=1;raggio<=20;raggio++) {
+        circoferenza= (2*3.14)*raggio;
+        printf("la circonferenza e': %.2f || ", circoferenza);
+        area = raggio*raggio*(3.14);
+        printf("l'area del cerchio e': %.2f \n", area);
     }
+}
 
